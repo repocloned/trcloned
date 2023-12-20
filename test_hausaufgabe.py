7463f4a79dc69b462808a57142052a5a0e5ad47c
gruenauerin <151530586+gruenauerin@users.noreply.github.com>
2023-12-20 16:24:22 +0100

    Update test_hausaufgabe.py

--- test_hausaufgabe.py
+++ test_hausaufgabe.py
@@ -70,7 +70,8 @@ class TestAufgabeZwei():
         #Act
         result = groesste_n_zahlen_implementation_1(liste,5)
         #Assert
-        assert result == [6,7,8,9,10]
+        assert set(result) == {6,7,8,9,10}
+        assert len(result) == 5
 
 
     def test_findet_hoechste_5_implementation_2(self):
@@ -79,7 +80,8 @@ class TestAufgabeZwei():
         #Act
         result = groesste_n_zahlen_implementation_2(liste,5)
         #Assert
-        assert result == [6,7,8,9,10]
+        assert set(result) == {6,7,8,9,10}
+        assert len(result) == 5 
 
 class TestAufgabeFuenf():
     def test_richtige_rueckgabe(self):
