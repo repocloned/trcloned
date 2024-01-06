19477ff3f42688c89becd66d49a60579852f6ed4
stsi_ws23_15 <144602225+m4rkmich4@users.noreply.github.com>
2024-01-06 10:01:40 +0100

    Update test_hausaufgabe.py

--- test_hausaufgabe.py
+++ test_hausaufgabe.py
@@ -84,7 +84,7 @@ class TestAufgabeZwei():
         #Act
         result = groesste_n_zahlen_implementation_2(liste,5)
         #Assert
-        assert result == [6,7,8,9,10]
+        assert result == [10,9,8,7,6]
 
 class TestAufgabeFuenf():
     def test_richtige_rueckgabe(self):
