9142e5e4e0af09a442ae5258438ebe94bd9e36c6
kohlhl <lukas.kohlhase@msg.group>
2024-11-21 11:53:21 +0100

    rechner-123: Division hinzugefuegt und Anpassungen aus Vorlesung

--- calcs.py
+++ calcs.py
@@ -19,7 +19,7 @@ class Taschenrechner(BaseModel):
         return self.converter.str_to_int(a) - self.converter.str_to_int(b)
 
     def multiplikation(self, a, b):
-        return self.converter.str_to_int(a) * self.converter.str_to_int(b)
+        return self.converter.str_to_int(a) - self.converter.str_to_int(b)\
 
     def division(self,a,b):
         return self.converter.str_to_int(a) / self.converter.str_to_int(b)
