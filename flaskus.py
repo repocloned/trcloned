8cfa8a76996f0d59592c95ec8f14b0134ca6fda2
hassella85754 <hassella85754@th-nuernberg.de>
2024-11-24 15:50:46 +0100

    Update flaskus.py

--- flaskus.py
+++ flaskus.py
@@ -20,7 +20,7 @@ APP = create_app()
 
 @APP.route('/version')
 def version_request():
-    return {"Version": "4.1 -- Lara Haßel"}
+    return {"Version": "4.1 -- Lara Hassel"}
 
 @APP.route('/add', methods=['POST'])
 def add_request():
