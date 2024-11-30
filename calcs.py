18018d03ee43c5affdff3e7e073b5133c65d3c9d
Krissy1111 <medererkr80230@th-nuernberg.de>
2024-11-30 16:07:38 +0100

    Update calcs.py

--- calcs.py
+++ calcs.py
@@ -21,5 +21,8 @@ class Taschenrechner(BaseModel):
     def multiplikation(self, a, b):
         return self.converter.str_to_int(a) * self.converter.str_to_int(b)
 
+    def multiplikation(self, a, b):
+        return self.converter.str_to_int(a) * self.converter.str_to_int(b)
+
     def division(self, a, b):
         return self.converter.str_to_int(a) / self.converter.str_to_int(b)
